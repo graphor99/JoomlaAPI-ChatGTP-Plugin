4e17a5dd4d0dbd18932bbc12b45cb758422762b8
Pascal Potvin <graphordesign@hotmail.com>
2023-08-09 08:50:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import random
 
 from JoomlaAPI import JoomlaAPI
 
-# Initialize the JoomlaAPI class with the provided parameters
-joomla = JoomlaAPI(auth_apikey="API Joomla in the User section", 
-                   base_url="http://localhost/joomla", base_path="/api/index.php/v1")
+def create_dynamic_article(title, content, category_id, alias=None):
+    # Initialize the JoomlaAPI class with the provided parameters
+    joomla = JoomlaAPI(auth_apikey="c2hhMjU2Ojc4Mzo2Mjg2ZTg4OGNiNjJmZWI5NWJiOGUzZTVlMTdkZjVhMjUxZTA2N2YxZGViYTkxNDMzYjgyMTRhYTQzOTI2ZmU5", 
+                       base_url="http://localhost/joomla", base_path="/api/index.php/v1")
 
-# Test creating an article in the specific category with ID 5 (ui design)
-response_article = joomla.create_article(
-    title="Test Article in UI Design Category",
-    content="This is a test article created in the UI Design category using the JoomlaAPI class from ChatGPT.",
-    category_id=13,  # Using the specific category ID
-    alias="test-article-ui-design-category"
+    # Create the article with the provided details
+    response_article = joomla.create_article(title, content, category_id, alias)
+    return response_article
+
+# Sample execution (this can be replaced by dynamic values from ChatGPT or user input)
+response = create_dynamic_article(
+    title="Dynamic Test Article",
+    content="This is a dynamically created test article using the JoomlaAPI class from ChatGPT.",
+    category_id=13,  # Example category ID
+    alias="dynamic-test-article"
 )
 
-print(response_article)
+print(response)
