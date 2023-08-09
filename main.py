e80217db0ee18a2f96ea642b0e63a8f2b1dd3d0d
Pascal Potvin <graphordesign@hotmail.com>
2023-08-09 08:17:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+
 from JoomlaAPI import JoomlaAPI
 
-if __name__ == "__main__":
-    # Remplacez par votre clé API et l'URL de votre site Joomla
-    joomla = JoomlaAPI(api_key="c2hhMjU2OjQyNDplYjJlZjk2OTY2MmQ0YmVkNTViNWQ5NDU1ZGJjZWNmNjcxNTc2NzM0NjVlMTQ0Yjc2NzAzNzU5YTZmNGQ5NGEx", base_url="https://pascalpotvin.com/api/index.php/v1")
+# Initialize the JoomlaAPI class with the provided parameters
+joomla = JoomlaAPI(auth_apikey="API Joomla in the User section", 
+                   base_url="http://localhost/joomla", base_path="/api/index.php/v1")
 
-    # Créer un article
-    response_article = joomla.create_article(
-        title="Here's an article",
-        content="My text",
-        category_id=10,
-        alias="test33"
-    )
-   
-    print(response_article)
-    print(response_article.status_code)  # Affiche le code de statut HTTP
-    print(response_article.text)         # Affiche le contenu de la réponse
+# Test creating an article in the specific category with ID 5 (ui design)
+response_article = joomla.create_article(
+    title="Test Article in UI Design Category",
+    content="This is a test article created in the UI Design category using the JoomlaAPI class from ChatGPT.",
+    category_id=13,  # Using the specific category ID
+    alias="test-article-ui-design-category"
+)
 
+print(response_article)
