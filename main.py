3be547fe2bb197536aabd38da18e533b875474ec
Pascal Potvin <graphordesign@hotmail.com>
2023-08-09 22:36:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-import random
 
 from JoomlaAPI import JoomlaAPI
+import random
 
-def create_dynamic_article(title, content, category_id, alias=None):
-    # Initialize the JoomlaAPI class with the provided parameters
-    joomla = JoomlaAPI(auth_apikey="c2hhMjU2Ojc4Mzo2Mjg2ZTg4OGNiNjJmZWI5NWJiOGUzZTVlMTdkZjVhMjUxZTA2N2YxZGViYTkxNDMzYjgyMTRhYTQzOTI2ZmU5", 
-                       base_url="http://localhost/joomla", base_path="/api/index.php/v1")
+# Initialize the JoomlaAPI class with the provided parameters
+joomla = JoomlaAPI(auth_apikey="YOUR_API_KEY_HERE", 
+                   base_url="http://localhost/joomla", 
+                   base_path="/api/index.php/v1")
 
-    # Create the article with the provided details
-    response_article = joomla.create_article(title, content, category_id, alias)
-    return response_article
-
-# Sample execution (this can be replaced by dynamic values from ChatGPT or user input)
-response = create_dynamic_article(
-    title="Dynamic Test Article",
-    content="This is a dynamically created test article using the JoomlaAPI class from ChatGPT.",
-    category_id=13,  # Example category ID
-    alias="dynamic-test-article"
+# Test creating an article in a specific category
+response_article = joomla.create_article(
+    title="Test Article",
+    content="This is a test article content.",
+    category_id=2,  # Example category ID
+    alias=f"test-article-{random.randint(1, 10000)}",
+    language="en-GB",
+    metakey="example, test, article",
+    metadesc="This is a test article description."
 )
 
-print(response)
+print(response_article)
+
+# List all articles
+articles = joomla.list_articles()
+print(articles)
+
+# If you have an article ID from previous operations, you can use the following methods:
+
+# Get specific article details
+# article_details = joomla.get_article(ARTICLE_ID_HERE)
+# print(article_details)
+
+# Update an article
+# updated_data = {
+#     "title": "Updated Title",
+#     "articletext": "Updated content for the article."
+# }
+# update_response = joomla.update_article(ARTICLE_ID_HERE, updated_data)
+# print(update_response)
+
+# Delete an article
+# delete_response = joomla.delete_article(ARTICLE_ID_HERE)
+# print(delete_response)
